4cb019bcefe04756e0565687613e782b3d022e31
Victor Pereira <victor.braga@skyone.solutions>
2019-04-29 17:49:57 -0300

    add new transport

--- nameko_zipkin/transport.py
+++ nameko_zipkin/transport.py
@@ -71,3 +71,17 @@ class Transport(SharedExtension):
 
     def handle(self, encoded_span):
         self._handler.handle(encoded_span)
+
+class HttpTransport(BaseTransportHandler):
+
+    def get_max_payload_bytes(self):
+        return None
+
+    def send(self, encoded_span):
+        # The collector expects a thrift-encoded list of spans.
+        response = requests.post(
+            'http://localhost:9411/api/v1/spans',
+            data=encoded_span,
+            headers={'Content-Type': 'application/x-thrift'},
+        )
+        return response
