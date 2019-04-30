90e2dedd695a59656ac1e172ef73c19ba209b203
Victor Pereira <victor.braga@skyone.solutions>
2019-04-30 16:55:27 -0300

    add decorator function

---
+++ nameko_zipkin/decorator_zipkin.py
@@ -0,0 +1,22 @@
+from nameko_zipkin.transport import HttpTransport
+from nameko_zipkin import monkey_patch
+from py_zipkin import zipkin
+
+
+def decorator_http_transport(url, service_name, span_name):
+    def decorator_zipkin(func):
+        def wrap(*original_args, **original_kwargs):
+            if not original_args[0].zipkin1:
+                print('trace')
+                handler = HttpTransport(url).handle
+                monkey_patch(handler)
+                with zipkin.zipkin_server_span(service_name,
+                                            span_name,
+                                            sample_rate=100.,
+                                            transport_handler=handler):
+                    func(*original_args, **original_kwargs)
+            else:
+                func(*original_args, **original_kwargs)
+        return wrap
+
+    return decorator_zipkin
