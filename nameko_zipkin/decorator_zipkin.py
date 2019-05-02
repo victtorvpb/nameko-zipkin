b0027aa926f57fcade97e1807893a6290d9d3163
Victor Pereira <victor.braga@skyone.solutions>
2019-05-02 09:32:58 -0300

    check if exsit atribute zipkin_nameko

--- nameko_zipkin/decorator_zipkin.py
+++ nameko_zipkin/decorator_zipkin.py
@@ -6,8 +6,7 @@ from py_zipkin import zipkin
 def decorator_http_transport(url, service_name, span_name):
     def decorator_zipkin(func):
         def wrap(*original_args, **original_kwargs):
-            if not original_args[0].zipkin1:
-                print('trace')
+            if not original_args[0].zipkin_nameko:
                 handler = HttpTransport(url).handle
                 monkey_patch(handler)
                 with zipkin.zipkin_server_span(service_name,
